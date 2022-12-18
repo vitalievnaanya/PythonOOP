de07d8b533ba255ec43c8247c16ca8fbe685f60e
Anna Vitalievna Dermenzhi <114215172+vitalievnaanya@users.noreply.github.com>
2022-12-18 15:36:33 +0200

    Add files via upload

---
+++ glass.py
@@ -0,0 +1,26 @@
+class Glass:
+    content = 0
+
+    def __init__(self):
+        self.capacity = 250
+
+    def fill(self, ml):
+        if ml > self.capacity - self.content:
+            return f'Cannot add {ml} ml'
+        self.content += ml
+        return f'Glass filled with {ml} ml'
+
+    def empty(self):
+        self.content = 0
+        return f'Glass is now empty'
+
+    def info(self):
+        return f'{self.capacity - self.content} ml left'
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
