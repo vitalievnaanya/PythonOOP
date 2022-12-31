24855b566280f593e1aae2475513c4ffbaa2c5a4
Anna Vitalievna Dermenzhi <114215172+vitalievnaanya@users.noreply.github.com>
2022-12-31 17:28:03 +0200

    Delete ebcapsulation_exercise/wild_cat_zoo/project directory

--- ebcapsulation_exercise/wild_cat_zoo/project/caretaker.py
+++
@@ -1,6 +0,0 @@
-from wild_cat_zoo.project.worker import Worker
-
-
-class Caretaker(Worker):
-    def __init__(self, name, age, salary):
-        super().__init__(name, age, salary)
--- ebcapsulation_exercise/wild_cat_zoo/project/cheetah.py
+++
@@ -1,6 +0,0 @@
-from wild_cat_zoo.project.animal import Animal
-
-
-class Cheetah(Animal):
-    def __init__(self, name, gender, age):
-        super().__init__(name, gender, age, 60)
